39ac56f15a7709be59f8759e7c57c514e858b87c
codernaija11 <66638496+codernaija11@users.noreply.github.com>
2021-03-16 16:03:50 +0100

    committing new file

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, qui
+        doloremque asperiores similique impedit, quos et itaque dolorum
+        repellendus, ducimus eaque esse vero facilis nam rerum eos. Quo, at
+        asperiores.
+      </p>
+    </section>
+  );
+};
+
+export default About;
